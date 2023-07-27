ae21f28e79793a2300892dcf3c16c4b9c5a1e7fb
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-07-27 13:45:00 -0400

    Added comments and cleaned up whitespace.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs/promises');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
-async function main() {
+async function main() { // Get the user's input
     const questions = [
         {
             type: 'input',
@@ -28,10 +28,10 @@ async function main() {
         }
     ];
 
-    const answers = await inquirer.prompt(questions);
+    const answers = await inquirer.prompt(questions); // Get the user's input and store it in the answers object
     let shapeInstance;
 
-    switch(answers.shape) {
+    switch(answers.shape) { // Determine the shape instance based on the user's input
         case 'circle':
             shapeInstance = new Circle();
             break;
@@ -50,13 +50,14 @@ async function main() {
         textYPosition = '150'; // Adjusted value for triangle
     }
 
-    const svgContent = `
+    // Determine the x-value for text position based on shape
+    const svgContent = ` 
         <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             ${shapeInstance.render()}
             <text x="150" y="${textYPosition}" font-family="Arial" font-size="45" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>
         </svg>
     `;
-
+    // Write the svg content to the file
     await fs.writeFile('./examples/logo.svg', svgContent);
     console.log("Generated logo.svg");
 }
