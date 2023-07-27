e42497080fa78c1334c31764566731bc3d40197f
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-07-27 12:42:42 -0400

    Change somethings around. Updated the code in the shapes.js and index.js files and fixed issues with ES Modules.

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-// index.js
-const chalk = require('chalk');
-const { promptUser } = require('./lib/userInput');
-const { Triangle, Circle, Square } = require('./lib/shapes');
-const { saveSVGToFile } = require('./lib/fileWriter');
+import inquirer from 'inquirer';
+import fs from 'fs/promises';  // Using the promise-based version of fs for async/await operations if needed
+import { Triangle, Circle, Square } from './lib/shapes.js';
+
 
 async function main() {
-  console.log(chalk.blue('Welcome to the Logo Generator!'));
+    const questions = [
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:',
+            validate: input => (input.length <= 3) ? true : "Enter up to three characters only!"
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a color keyword (OR a hexadecimal number) for the text:',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select a shape:',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a color keyword (OR a hexadecimal number) for the shape:',
+        }
+    ];
 
-  try {
-    const { text, textColor, shape, shapeColor } = await promptUser();
-    let shapeObj;
+    const answers = await inquirer.prompt(questions);
+    let shapeInstance;
 
-    if (shape === 'circle') {
-      shapeObj = new Circle();
-    } else if (shape === 'triangle') {
-      shapeObj = new Triangle();
-    } else if (shape === 'square') {
-      shapeObj = new Square();
+    switch(answers.shape) {
+        case 'circle':
+            shapeInstance = new Circle();
+            break;
+        case 'triangle':
+            shapeInstance = new Triangle();
+            break;
+        case 'square':
+            shapeInstance = new Square();
+            break;
     }
 
-    if (shapeObj) {
-      shapeObj.setColor(shapeColor);
-      const svg = shapeObj.render();
-      saveSVGToFile(svg, 'logo.svg');
-    } else {
-      console.error(chalk.red('Invalid shape selection'));
-    }
-  } catch (error) {
-    console.error(chalk.red('An error occurred:', error));
-  }
+    shapeInstance.setColor(answers.shapeColor);
+    const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            ${shapeInstance.render()}
+            <text x="150" y="105" font-family="Arial" font-size="45" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>
+        </svg>
+    `;
+
+    fs.writeFileSync('logo.svg', svgContent);
+    console.log("Generated logo.svg");
 }
 
 main();
